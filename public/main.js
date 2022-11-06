577909670ebf2113c3ec3cb409e99a7ce9fb6f2d
Sarah Bouayad <112355619+sarebouayad@users.noreply.github.com>
2022-11-06 18:34:46 -0500

    updated version, philadelphia collection fixed

--- public/main.js
+++ public/main.js
@@ -31,48 +31,14 @@ Array.from(checkBox).forEach(function(element) {
       });
 });
 
-
-// Array.from(checkBox).forEach(function(element) {
-//   element.addEventListener('click', function(){
-//     const artistName = this.parentNode.parentNode.childNodes[1].innerText
-//     const msg = this.parentNode.parentNode.childNodes[3].innerText
-//   //  dataset (data attribute) and heart is the variable already set
-//   // comparing it to true so it can be boolean
-//     const heartIcon = this.dataset.heart === "true"
-
-//     // if(this.dataset.heart){
-//     //   heartIcon = true
-//     // } else {
-//     //   heartIcon = false
-//     // }
-    
-//     fetch('favorites', {
-//       method: 'put',
-//       headers: {'Content-Type': 'application/json'},
-//       body: JSON.stringify({
-//         'artistName': artistName,
-//         'msg': msg,
-//         'heart': heartIcon
-//       })
-//     })
-//     .then(response => {
-//       if (response.ok) return response.json()
-//     })
-//     .then(data => {
-//       console.log(data)
-//       window.location.reload(true)
-//     })
-//   });
-// });
-
-
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const jobListing = this.parentNode.parentNode.childNodes[3].innerText
         const connect = this.parentNode.parentNode.childNodes[5].innerText
         const msg = this.parentNode.parentNode.childNodes[7].innerText
-
+        console.log(name)
+        console.log(msg)
         fetch('messages', {
           method: 'delete',
           headers: {
